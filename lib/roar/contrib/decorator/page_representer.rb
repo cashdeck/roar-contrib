439cb1fe2a3de887ea94c4b6a48606d4479a02e5
Wayne Robinson <wayne.robinson@gmail.com>
2021-03-26 17:27:03 +1000

    Bump Implementation of PageRepresenter's Links

--- lib/roar/contrib/decorator/page_representer.rb
+++ lib/roar/contrib/decorator/page_representer.rb
@@ -50,19 +50,19 @@ module Roar
 
           property :total_entries, exec_context: :decorator
 
-          link :self do |opts = {}|
-            page_url opts.merge(:page => current_page, :per_page => per_page)
+          link :self do |opts|
+            page_url (opts || {}).merge(:page => current_page, :per_page => per_page)
           end
 
-          link :next do |opts = {}|
+          link :next do |opts|
             if next_page
-              page_url opts.merge(:page => next_page, :per_page => per_page)
+              page_url (opts || {}).merge(:page => next_page, :per_page => per_page)
             end
           end
 
-          link :previous do |opts = {}|
+          link :previous do |opts|
             if previous_page
-              page_url opts.merge(:page => previous_page, :per_page => per_page)
+              page_url (opts || {}).merge(:page => previous_page, :per_page => per_page)
             end
           end
         end
